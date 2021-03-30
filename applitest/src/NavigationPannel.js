1c78caa42709ba4277b3985b22d8c915d6210e3e
MRVNY <MRVNY@users.noreply.github.com>
2021-03-30 19:56:35 +0200

    React

---
+++ applitest/src/NavigationPannel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavigationPannel extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <p></p>
+    }
+}
+
+export default NavigationPannel;
